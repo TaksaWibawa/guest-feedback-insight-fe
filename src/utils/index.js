6004f9baa508e8bf325f0bf2e7b716cabfc3da6a
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-26 17:11:36 +0800

    chore: add format date utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { addDotsToNumber } from './addDotsToNumber';
 export { capitalizeSentence } from './capitalizeSentence';
 export { getPathnameParts } from './getPathnameParts';
 export { setScoreColor } from './setScoreColor';
+export { formatDate } from './formatDate';
