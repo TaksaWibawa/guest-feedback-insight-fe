8f572a9db2093009ffbdb2e2c7f6fbe6e4117cd1
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-26 17:15:12 +0800

    chore: modify usePathnameToArray and add new color in theme

--- src/hooks/usePathnameToArray.js
+++ src/hooks/usePathnameToArray.js
@@ -1,12 +1,12 @@
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export function usePathnameToArray() {
 	const { pathname } = useLocation();
 	const [pathnameParts, setPathnameParts] = useState([]);
 
 	useEffect(() => {
-		const parts = pathname.split("/").map((part) => part || "home");
+		const parts = pathname.split('/').map((part) => part || 'dashboard');
 		setPathnameParts(parts);
 	}, [pathname]);
 
