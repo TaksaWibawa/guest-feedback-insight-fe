2f1ea2151307f8a439fa6c8e858ffda891b6bf6a
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-26 17:19:27 +0800

    chore: modify dropdown and integrate to api call

---
+++ src/components/ui/dropdown/dropdown-custom/index.jsx
@@ -0,0 +1,28 @@
+import { Select } from '@chakra-ui/react';
+
+export function DropdownCustom({ width, options, defaultValue, onChange, isLoading, ...props }) {
+	return (
+		<Select
+			size={'sm'}
+			maxW={'9rem'}
+			w={width}
+			defaultValue={defaultValue}
+			onChange={(event) => {
+				onChange(event.target.value);
+			}}
+			isDisabled={isLoading}
+			{...props}
+		>
+			{options?.map((option) => {
+				return (
+					<option
+						key={option.value}
+						value={option.value}
+					>
+						{option.label}
+					</option>
+				);
+			})}
+		</Select>
+	);
+}
--- src/components/ui/dropdown/index.js
+++ src/components/ui/dropdown/index.js
@@ -1,1 +1,1 @@
-export { Dropdown } from "./Dropdown";
+export { DropdownCustom } from './dropdown-custom';
