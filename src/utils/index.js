a1672986f470c6be112e0d4de76e66afc1b4c220
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-04-08 05:04:53 +0800

    chore: remove setScoreColor util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 export { addDotsToNumber } from './addDotsToNumber';
 export { capitalizeSentence } from './capitalizeSentence';
 export { getPathnameParts } from './getPathnameParts';
-export { setScoreColor } from './setScoreColor';
 export { formatDate } from './formatDate';
